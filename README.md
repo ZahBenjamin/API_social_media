9ef2643d28b85a3366be6a6dbbe4f75f4cabc2bc
ZahBenjamin <bz@benzah.com>
2022-04-23 13:13:08 -0400

    Final touches, README

---
+++ README.md
@@ -0,0 +1,24 @@
+# Social Media API - A MongooseDB App
+
+## License
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Table of Contents
+1. [Description](#description)
+2. [Installation](#installation)
+3. [Demo](#demo)
+4. [Usage](#usage)
+
+## Description
+This is a working example of how to incorporate MongooseDB with NodeJS. MySQL is a tool for relational schema, MongoDB is not relational on it's own but Mongoose incorporates some features to MongoDB that offers some of what MySQL sets out to do. 
+
+## Installation
+    npm install
+
+## Demo
+[Video here](https://www.youtube.com/watch?v=iw6dPg6rW2A)
+
+
+## Usage
+    Make sure to run npm install and that the package.json is set to start with the server.js file.
--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -51,7 +51,7 @@ module.exports = {
       })
       .catch((err) => res.status(500).json(err));
   },
-  // delete thought --WORKS(DOESN'T DELETE IN USERDB)
+  // delete thought --WORKS
   deleteThought(req, res) {
     Thought.findOneAndDelete({ _id: req.params.thoughtId })
       .then((thought) => {
@@ -60,7 +60,7 @@ module.exports = {
         }
 
         return User.findOneAndUpdate(
-          { _id: req.body.userId }, // should be the user id
+          { _id: req.body.userId }, 
           { $pull: { thoughts: req.params.thoughtId } },
           {new: true }
         );
@@ -91,7 +91,7 @@ module.exports = {
             res.status(400).json(err);
         });
 },
-  // remove reaction --TESTING
+  // remove reaction --WORKS
   removeReaction(req, res) {
     Thought.findOneAndUpdate(
       { _id: req.params.thoughtId },
@@ -107,21 +107,3 @@ module.exports = {
       .catch((err) => res.status(500).json(err));
   },
 };
-
-// /api/thoughts
-
-// GET to get all thoughts
-
-// GET to get a single thought by its _id
-
-// POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
-
-// PUT to update a thought by its _id
-
-// DELETE to remove a thought by its _id
-
-// /api/thoughts/:thoughtId/reactions
-
-// POST to create a reaction stored in a single thought's reactions array field
-
-// DELETE to pull and remove a reaction by the reaction's reactionId value
